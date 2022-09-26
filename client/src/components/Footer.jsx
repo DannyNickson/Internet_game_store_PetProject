08e8fd45572b7f1db8d1b7968710b03a2f15a4e9
anny <puchkovdd@gmail.com>
2022-09-26 16:56:14 +0300

    added header and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Its Footer!!
-        </div>
+        <footer>
+            <div className="footer__container">
+                <div className="copyrights__container"></div>
+                <div className="footer__menu">
+                    <nav>
+                        <li>About me</li>
+                        <li>Jobs</li>
+                        <li>MyShopWorks</li>
+                        <li>Support</li>
+                    </nav>
+                </div>
+                <div className="contacts">
+                    <img src="" alt="telegram" className="telegram" />
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import '../styles/main.css'
 const Header = () => {
     return (
-        <div>
-            Its header!
-        </div>
+        <header>
+            <div className="header__container">
+                <div className="top__menu">
+                    <div className="login__link">login</div>
+                    <div className="language__select">language</div>
+                </div>
+                <div className="low__menu">
+                    <div className="logo"><a href="http://localhost:3000/"><img src="" alt="LOGO" /></a></div>
+                    <div className="nav__bar">
+                        <nav>
+                            <li>Store</li>
+                            <li>Comunity</li>
+                            <li>About</li>
+                            <li>Support</li>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 };
 
