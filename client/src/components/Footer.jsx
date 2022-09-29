59d7e9a9b6891210307c7b1652878999f5511c80
anny <puchkovdd@gmail.com>
2022-09-29 13:54:01 +0300

    footer component completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-
+import React from "react";
+import telegramLogo from "../img/Telegram_logo.svg.webp";
+import logo from "../img/LOGO.png";
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer__container">
-                <div className="copyrights__container">
-                    I dont have any copyrights.
-                </div>
-                <div className="footer__menu">
-                    <nav>
-                        <li>About me</li>
-                        <li>Jobs</li>
-                        <li>MyShopWorks</li>
-                        <li>Support</li>
-                    </nav>
-                </div>
-                <div className="contacts">
-                    <img src="" alt="telegram" className="telegram" />
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="footer__container">
+        <div className="copyrights__container">
+          <div className="copyrights_text">I dont have any copyrights.</div>
+          <div className="copyrights_logo">
+            <img src={logo} alt="mini_logo" />
+          </div>
+        </div>
+        <div className="footer__menu">
+          <a href="/">About me</a>
+          <a href="/">Jobs</a>
+          <a href="/">MyShopWorks</a>
+          <a href="/">Support</a>
+          <div className="contacts">
+            <div className="contact_text">Contact me:</div>
+            <a href="https://telegram.me/danilpuchkov" target="_blank" rel="noopener noreferrer">
+              <img src={telegramLogo} alt="telegram" className="telegram" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
