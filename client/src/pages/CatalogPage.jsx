a4b1ae40cd351a8ba5936517e0a0a1bcec582804
anny <puchkovdd@gmail.com>
2022-10-05 13:09:47 +0300

    complited html structure and css styled slider

--- client/src/pages/CatalogPage.jsx
+++ client/src/pages/CatalogPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/pagesStyles/CatalogPageStyle.css";
+import dotaLogo from "../img/doca2.png";
 const CatalogPage = () => {
   return (
     <div className="catalog__page__main">
@@ -144,136 +145,148 @@ const CatalogPage = () => {
                 </div>
               </div>
             </div>
-            <div className="top__slider">
-              <div className="slider__title">Featured & Recommended</div>
-              <div className="slider__main__block">
-                <div className="slide active">
-                  <a href="/">
-                    <div className="main__img">
-                      <img src="" alt="" />
-                    </div>
-                    <div className="secondary__block">
-                      <div className="game__title">DOKA 2</div>
-                      <div className="screenshots__group">
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
+            <div className="sliders">
+              <div className="top__slider">
+                <div className="slider__title">Featured & Recommended</div>
+                <div className="slider__main__block">
+                  <div className="slide active">
+                    <a href="/">
+                      <div className="main__img">
+                        <img src={dotaLogo} alt="mainslidephoto" />
                       </div>
-                      <div className="game__status">Now Available</div>
-                      <div className="game__tag">Top Seller</div>
-                      <div className="prive__platform__block">
-                        <div className="price">Free to Play</div>
-                        <div className="platforms"></div>
+                      <div className="secondary__block">
+                        <div className="game__title">DOKA 2</div>
+                        <div className="screenshots__group">
+                          <img src={dotaLogo} alt="screenshot1" />
+                          <img src={dotaLogo} alt="screenshot2" />
+                          <img src={dotaLogo} alt="screenshot3" />
+                          <img src={dotaLogo} alt="screenshot4" />
+                        </div>
+                        <div className="game__status">Now Available</div>
+                        <div className="game__tag">Top Seller</div>
+                        <div className="prive__platform__block">
+                          <div className="price">Free to Play</div>
+                          <div className="platforms">
+                            <span className="windows_icon"></span>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </a>
-                </div>
-                <div className="slide">
-                  <a href="/">
-                    <div className="main__img">
-                      <img src="" alt="" />
-                    </div>
-                    <div className="secondary__block">
-                      <div className="game__title">CS:GO</div>
-                      <div className="screenshots__group">
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                      </div>
-                      <div className="game__status">Now Available</div>
-                      <div className="game__tag">Top Seller</div>
-                      <div className="prive__platform__block">
-                        <div className="price">Free to Play</div>
-                        <div className="platforms"></div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="slider__arrows"></div>
-              <div className="slider__dots"></div>
-            </div>
-            <div className="lower__slider">
-              <div className="slider__title">Special Offers</div>
-              <div className="browse__more__button">
-                <a href="/">Browse More</a>
-              </div>
-              <div className="slider__main__block">
-                <div className="slide">
-                  <div className="bigger__block">
-                    <div className="block__img">
-                      <img src="" alt="" />
-                    </div>
-                    <div className="slider__game__title">DOKA 2</div>
-                    <div className="special__offer">
-                      Offer ends 10 Oct @ 8:00pm
-                    </div>
-                    <div className="slider__game__slider">
-                      <div className="discount">-35%</div>
-                      <div className="price__block">
-                        <div className="old__price">22$</div>
-                        <div className="new__price">15$</div>
-                      </div>
-                    </div>
+                    </a>
                   </div>
-                  <div className="normal__slide__block">
-                    <img src="" alt="" />
-                    <div className="special__offer">
-                      Offer ends 10 Oct @ 8:00pm
-                    </div>
-                    <div className="slider__game__slider">
-                      <div className="discount">-35%</div>
-                      <div className="price__block">
-                        <div className="old__price">22$</div>
-                        <div className="new__price">15$</div>
+                  <div className="slide">
+                    <a href="/">
+                      <div className="main__img">
+                        <img src="" alt="" />
                       </div>
-                    </div>
-                  </div>
-                  <div className="normal__slide__block">
-                    <img src="" alt="" />
-                    <div className="special__offer">
-                      Offer ends 10 Oct @ 8:00pm
-                    </div>
-                    <div className="slider__game__slider">
-                      <div className="discount">-35%</div>
-                      <div className="price__block">
-                        <div className="old__price">22$</div>
-                        <div className="new__price">15$</div>
+                      <div className="secondary__block">
+                        <div className="game__title">CS:GO</div>
+                        <div className="screenshots__group">
+                          <img src="" alt="" />
+                          <img src="" alt="" />
+                          <img src="" alt="" />
+                          <img src="" alt="" />
+                        </div>
+                        <div className="game__status">Now Available</div>
+                        <div className="game__tag">Top Seller</div>
+                        <div className="prive__platform__block">
+                          <div className="price">Free to Play</div>
+                          <div className="platforms"></div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="normal__slide__block">
-                    <img src="" alt="" />
-                    <div className="special__offer">
-                      Offer ends 10 Oct @ 8:00pm
-                    </div>
-                    <div className="slider__game__slider">
-                      <div className="discount">-35%</div>
-                      <div className="price__block">
-                        <div className="old__price">22$</div>
-                        <div className="new__price">15$</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="normal__slide__block">
-                    <img src="" alt="" />
-                    <div className="special__offer">
-                      Offer ends 10 Oct @ 8:00pm
-                    </div>
-                    <div className="slider__game__slider">
-                      <div className="discount">-35%</div>
-                      <div className="price__block">
-                        <div className="old__price">22$</div>
-                        <div className="new__price">15$</div>
-                      </div>
-                    </div>
+                    </a>
                   </div>
                 </div>
+                <div className="arrow__left">
+                  <div></div>
+                </div>
+                <div className="arrow__right">
+                  <div></div>
+                </div>
+                <div className="slider__dots">
+                  <div className="active"></div>
+                  <div></div>
+                </div>
               </div>
-              <div className="slider__arrows"></div>
-              <div className="slider__dots"></div>
+              <div className="lower__slider">
+                <div className="slider__title">Special Offers</div>
+                <div className="browse__more__button">
+                  <a href="/">Browse More</a>
+                </div>
+                <div className="slider__main__block">
+                  <div className="slide">
+                    <div className="bigger__block">
+                      <div className="block__img">
+                        <img src={dotaLogo} alt="screenshot" />
+                      </div>
+                      <div className="slider__game__title">DOKA 2</div>
+                      <div className="special__offer">
+                        Offer ends 10 Oct @ 8:00pm
+                      </div>
+                      <div className="slider__game__slider">
+                        <div className="discount">-35%</div>
+                        <div className="price__block">
+                          <div className="old__price">22$</div>
+                          <div className="new__price">15$</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="normal__slide__block">
+                      <img src="" alt="" />
+                      <div className="special__offer">
+                        Offer ends 10 Oct @ 8:00pm
+                      </div>
+                      <div className="slider__game__slider">
+                        <div className="discount">-35%</div>
+                        <div className="price__block">
+                          <div className="old__price">22$</div>
+                          <div className="new__price">15$</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="normal__slide__block">
+                      <img src="" alt="" />
+                      <div className="special__offer">
+                        Offer ends 10 Oct @ 8:00pm
+                      </div>
+                      <div className="slider__game__slider">
+                        <div className="discount">-35%</div>
+                        <div className="price__block">
+                          <div className="old__price">22$</div>
+                          <div className="new__price">15$</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="normal__slide__block">
+                      <img src="" alt="" />
+                      <div className="special__offer">
+                        Offer ends 10 Oct @ 8:00pm
+                      </div>
+                      <div className="slider__game__slider">
+                        <div className="discount">-35%</div>
+                        <div className="price__block">
+                          <div className="old__price">22$</div>
+                          <div className="new__price">15$</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="normal__slide__block">
+                      <img src="" alt="" />
+                      <div className="special__offer">
+                        Offer ends 10 Oct @ 8:00pm
+                      </div>
+                      <div className="slider__game__slider">
+                        <div className="discount">-35%</div>
+                        <div className="price__block">
+                          <div className="old__price">22$</div>
+                          <div className="new__price">15$</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="slider__arrows"></div>
+                <div className="slider__dots"></div>
+              </div>
             </div>
           </div>
         </div>
